68e08dd32c6478e6ab5f3e6b191e2bd1611f6674
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-09-18 15:07:55 -0700

    FormsModule library added

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-angular-app');
+  protected readonly name = 'Aaron Topping'
 }
